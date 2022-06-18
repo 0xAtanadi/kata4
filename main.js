6af87b478d3380a7356b191f54def93e4e41fa9e
0xAtanadi <107759002+0xAtanadi@users.noreply.github.com>
2022-06-18 19:11:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 4.
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs: a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example: 
 let testList = ["aabb", "abcd", "bbaa", "dada"];
 function anagrams(originalWord, wordsInArray) {
   anagramList = [];
